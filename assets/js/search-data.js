b0c7a82acafc0ffc2a3bfec5da4b1020c065fe01
gyuwon12 <99570746+gyuwon12@users.noreply.github.com>
2026-01-21 07:14:09 +0000

    Deploying to gh-pages from @ gyuwon12/gyuwon12.github.io@577d262a3124dcf11f95193a72d76f75a9c0b7a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,6 +479,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/gyuwon12", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=o9AuSVIAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
